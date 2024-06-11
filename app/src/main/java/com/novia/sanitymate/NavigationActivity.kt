4a80520739ae00421c857e96b42b0f4f94c10650
Novia Natasya <novianatasya@student.telkomuniversity.ac.id>
2024-06-12 01:26:14 +0700

    Control onboarding screen

--- app/src/main/java/com/novia/sanitymate/NavigationActivity.kt
+++ app/src/main/java/com/novia/sanitymate/NavigationActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OnboardingActivity : AppCompatActivity() {
+class NavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.onboarding_screen1)
+        setContentView(R.layout.activity_navigation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/novia/sanitymate/ViewPagerAdapter.kt
@@ -0,0 +1,62 @@
+package com.novia.sanitymate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import org.w3c.dom.Text;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+    Context context;
+
+    int sliderAllImages[] = {R.drawable.screen1, R.drawable.screen2};
+    int sliderAllTitle[] = {R.string.screen1, R.string.screen2};
+    int sliderAllDesc[] = {R.string.screen1desc, R.string.screen2desc};
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return sliderAllTitle.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (FrameLayout) object;
+
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position){
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slider_screen, container, false);
+
+        ImageView sliderImage = (ImageView) view.findViewById(R.id.sliderImage);
+        TextView sliderTitle = (TextView) view.findViewById(R.id.sliderTitle);
+        TextView sliderDesc = (TextView) view.findViewById(R.id.sliderDesc);
+
+        sliderImage.setImageResource(sliderAllImages[position]);
+        sliderTitle.setText(this.sliderAllTitle[position]);
+        sliderDesc.setText(this.sliderAllTitle[position]);
+
+        container.addView(view);
+        return view;
+
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object){
+        container.removeView((FrameLayout)object);
+    }
+}
